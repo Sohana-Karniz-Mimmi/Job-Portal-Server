eeff6345ee5b3dc50640ceabb4c6f2587f79ba89
Sohana-Karniz-Mimmi <sohanasheikhmimmi@gmail.com>
2024-05-14 10:01:59 +0600

    Vercel Deploy Setup Done

--- index.js
+++ index.js
@@ -12,11 +12,10 @@ const port = process.env.PORT || 5000;
 app.use(
   cors({
     origin: [
-      'http://localhost:5173',
-      'http://localhost:5174',
-      'https://job-portal-3285e.web.app',
-      'https://job-portal-3285e.firebaseapp.com'
-      // 'https://solosphere.web.app',
+      "http://localhost:5173",
+      "http://localhost:5174",
+      "https://job-portal-3285e.web.app",
+      "https://job-portal-3285e.firebaseapp.com",
     ],
     credentials: true,
   })
@@ -30,7 +29,6 @@ const logger = async (req, res, next) => {
   next();
 };
 
-
 // verify jwt middleware
 const verifyToken = async (req, res, next) => {
   // Get token
@@ -42,19 +40,17 @@ const verifyToken = async (req, res, next) => {
 
   jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
     //if token is not valid... error
-    if(err) {
-      return res.status(401).send({ message: 'Unauthorized access' });
+    if (err) {
+      return res.status(401).send({ message: "Unauthorized access" });
     }
 
     // If token is valid
-    // console.log('value in token', decoded);
-    req.user = decoded
+    console.log("value in token", decoded);
+    req.user = decoded;
     next();
   });
 
-
   // next()
-
 };
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.2xcjib6.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
@@ -86,26 +82,28 @@ async function run() {
       res
         .cookie("token", token, {
           httpOnly: true,
-          secure: false,
-          // secure: process.env.NODE_ENV === 'production',
-          // sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
+          // secure: false,
+          secure: process.env.NODE_ENV === "production" ? true : false,
+          sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
         })
         .send({ success: true });
     });
 
     // Clear token on logout
-    app.post('/logout', async(req, res) => {
-      const user = req.body
+    app.post("/logout", async (req, res) => {
+      const user = req.body;
       // console.log('User logged out');
-      res.clearCookie('token', {
-        httpOnly: true,
-          // secure: process.env.NODE_ENV === 'production',
-          // sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
+      res
+        .clearCookie("token", {
+          httpOnly: true,
+          secure: process.env.NODE_ENV === "production" ? true : false,
+          sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
           maxAge: 0,
-      }).send({message : true})
-    })
+        })
+        .send({ message: true });
+    });
 
-   // Get all jobs data from db
+    // Get all jobs data from db
     app.get(`/jobs`, logger, async (req, res) => {
       const cursor = jobPortalCollection.find();
       const result = await cursor.toArray();
@@ -119,36 +117,37 @@ async function run() {
       const result = await jobPortalCollection.findOne(query);
       res.send(result);
     });
-   
+
     // Save a apply data in db
-    app.post('/apply', async (req, res) => {
-      const applyData = req.body
+    app.post("/apply", async (req, res) => {
+      const applyData = req.body;
       // console.log(applyData);
 
       // check if its a duplicate request
       const query = {
         email: applyData.email,
         jobId: applyData.jobId,
-      }
-      const alreadyApplied = await applyJobCollection.findOne(query)
+      };
+      const alreadyApplied = await applyJobCollection.findOne(query);
       // console.log(alreadyApplied)
       if (alreadyApplied) {
-        return res
-          .status(400)
-          .send('You have already apply on this job.')
+        return res.status(400).send("You have already apply on this job.");
       }
 
-      const result = await applyJobCollection.insertOne(applyData)
+      const result = await applyJobCollection.insertOne(applyData);
 
       // update apply count in jobs collection
       const updateDoc = {
         $inc: { apply_count: 1 },
-      }
-      const jobQuery = { _id: new ObjectId(applyData.jobId) }
-      const updateApplyCount = await jobPortalCollection.updateOne(jobQuery, updateDoc)
+      };
+      const jobQuery = { _id: new ObjectId(applyData.jobId) };
+      const updateApplyCount = await jobPortalCollection.updateOne(
+        jobQuery,
+        updateDoc
+      );
       // console.log(updateApplyCount)
-      res.send(result)
-    })
+      res.send(result);
+    });
 
     // Save a job data in db
     app.post("/job", async (req, res) => {
@@ -158,18 +157,17 @@ async function run() {
       res.send(result);
     });
 
-     // get all jobs posted by a specific user
-     app.get('/jobs/:email', verifyToken, async (req, res) => {
-      const tokenEmail = req.user.email
-      const email = req.params.email
+    // get all jobs posted by a specific user
+    app.get("/jobs/:email", verifyToken, async (req, res) => {
+      const tokenEmail = req.user.email;
+      const email = req.params.email;
       if (tokenEmail !== email) {
-        return res.status(403).send({ message: 'forbidden access' })
+        return res.status(403).send({ message: "forbidden access" });
       }
-      const query = { 'buyer.email': email }
-      const result = await jobPortalCollection.find(query).toArray()
-      res.send(result)
-    })
-
+      const query = { "buyer.email": email };
+      const result = await jobPortalCollection.find(query).toArray();
+      res.send(result);
+    });
 
     // delete a job data from db
     app.delete("/job/:id", async (req, res) => {
@@ -181,65 +179,69 @@ async function run() {
     });
 
     // update a job in db
-    app.put('/job/:id', async (req, res) => {
-      const id = req.params.id
-      const jobData = req.body
-      const query = { _id: new ObjectId(id) }
-      const options = { upsert: true }
+    app.put("/job/:id", async (req, res) => {
+      const id = req.params.id;
+      const jobData = req.body;
+      const query = { _id: new ObjectId(id) };
+      const options = { upsert: true };
       const updateDoc = {
         $set: {
           ...jobData,
         },
-      }
-      const result = await jobPortalCollection.updateOne(query, updateDoc, options)
-      res.send(result)
-    })
+      };
+      const result = await jobPortalCollection.updateOne(
+        query,
+        updateDoc,
+        options
+      );
+      res.send(result);
+    });
 
     // get all apply for a user by email from db
-    app.get('/my-apply/:email', verifyToken, async (req, res) => {
-      const email = req.params.email
-      const query = { email }
-      const result = await applyJobCollection.find(query).toArray()
-      res.send(result)
-    })
+    app.get("/my-apply/:email", verifyToken, async (req, res) => {
+      const email = req.params.email;
+      const query = { email };
+      const result = await applyJobCollection.find(query).toArray();
+      res.send(result);
+    });
 
     // Get all jobs data from db for pagination
-    app.get('/all-jobs', async (req, res) => {
-      const size = parseInt(req.query.size)
-      const page = parseInt(req.query.page) - 1
-      const filter = req.query.filter
-      const sort = req.query.sort
-      const search = req.query.search
+    app.get("/all-jobs", async (req, res) => {
+      const size = parseInt(req.query.size);
+      const page = parseInt(req.query.page) - 1;
+      const filter = req.query.filter;
+      const sort = req.query.sort;
+      const search = req.query.search;
       // console.log(size, page)
 
       let query = {
-        job_title: { $regex: search, $options: 'i' },
-      }
-      if (filter) query.category = filter
-      let options = {}
-      if (sort) options = { sort: { deadline: sort === 'asc' ? 1 : -1 } }
+        job_title: { $regex: search, $options: "i" },
+      };
+      if (filter) query.category = filter;
+      let options = {};
+      if (sort) options = { sort: { deadline: sort === "asc" ? 1 : -1 } };
       const result = await jobPortalCollection
         .find(query, options)
         .skip(page * size)
         .limit(size)
-        .toArray()
+        .toArray();
 
-      res.send(result)
-    })
+      res.send(result);
+    });
 
-     // Get all jobs data count from db
-     app.get('/jobs-count', async (req, res) => {
-      const filter = req.query.filter
-      const search = req.query.search
+    // Get all jobs data count from db
+    app.get("/jobs-count", async (req, res) => {
+      const filter = req.query.filter;
+      const search = req.query.search;
       let query = {
-        job_title: { $regex: search, $options: 'i' },
-      }
-      if (filter) query.category = filter
-      const count = await jobPortalCollection.countDocuments(query)
+        job_title: { $regex: search, $options: "i" },
+      };
+      if (filter) query.category = filter;
+      const count = await jobPortalCollection.countDocuments(query);
 
-      res.send({ count })
-    })
-    
+      res.send({ count });
+    });
+
     // Send a ping to confirm a successful connection
     // await client.db("admin").command({ ping: 1 });
     console.log(
@@ -253,7 +255,7 @@ async function run() {
 run().catch(console.dir);
 
 app.get("/", (req, res) => {
-  res.send("Assignment 11 Server is running");
+  res.send("Job Portal Server is running");
 });
 
 app.listen(port, () => {
